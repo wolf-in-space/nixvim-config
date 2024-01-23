77875395480aae5ceea5962c4b8f5d48929a166c
wolf-in-space <jakob@wolf-in-space.dev>
2024-01-23 13:24:34 +0100

    remove redundant keymaps

--- config/default.nix
+++ config/default.nix
@@ -17,21 +17,5 @@
       };
     };
 
-    keymaps = with extraLib; [
-      (key "-" "Oil --float")
-      (key "<C-s>" "w")
-      (key "<leader>f" "Telescope frecency workspace=CWD")
-      (key "<leader>p" "Telescope projects")
-
-      # Start tmux panes / windows with nvim cwd
-      (key "<leader>ty" "!tmux splitw -h")
-      (key "<leader>tx" "!tmux splitw -v")
-      (key "<leader>ta" "!tmux new-window")
-
-      # Start tmux lazygit panes / windows with nvim cwd
-      (key "<leader>tgy" "!tmux splitw -h lazygit")
-      (key "<leader>tgx" "!tmux splitw -v lazygit")
-      (key "<leader>tga" "!tmux new-window lazygit")
-    ];
   };
 }
